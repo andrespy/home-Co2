fb61400caeed15ca5de98162754c657480a1472d
andrespy <andresprietoyanes@gmail.com>
2020-11-27 18:07:03 +0100

    🚀 Basic Arduino read

---
+++ Software/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/andres/repos/home-Co2/Software/src/hello.cpp" "/Users/andres/repos/home-Co2/Software/cmake-build-uno/CMakeFiles/Z_DUMMY_TARGET.dir/src/hello.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "/Users/andres/.platformio/packages/framework-arduino-avr/cores/arduino"
+  "/Users/andres/.platformio/packages/framework-arduino-avr/variants/standard"
+  "/Users/andres/.platformio/packages/framework-arduino-avr/libraries/EEPROM/src"
+  "/Users/andres/.platformio/packages/framework-arduino-avr/libraries/HID/src"
+  "/Users/andres/.platformio/packages/framework-arduino-avr/libraries/SPI/src"
+  "/Users/andres/.platformio/packages/framework-arduino-avr/libraries/SoftwareSerial/src"
+  "/Users/andres/.platformio/packages/framework-arduino-avr/libraries/Wire/src"
+  "/Users/andres/.platformio/packages/toolchain-atmelavr/lib/gcc/avr/5.4.0/include"
+  "/Users/andres/.platformio/packages/toolchain-atmelavr/lib/gcc/avr/5.4.0/include-fixed"
+  "/Users/andres/.platformio/packages/toolchain-atmelavr/avr/include"
+  "/Users/andres/.platformio/packages/tool-unity"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- Software/src/hello.cpp
+++ Software/src/hello.cpp
@@ -1,31 +1,96 @@
-//
-// Created by Andrés on 26/11/2020.
-//
+/*
+  Software serial multple serial test
 
+ Receives from the hardware serial, sends to software serial.
+ Receives from software serial, sends to hardware serial.
+
+ The circuit:
+ * RX is digital pin 2 (connect to TX of other device)
+ * TX is digital pin 3 (connect to RX of other device)
+
+ Note:
+ Not all pins on the Mega and Mega 2560 support change interrupts,
+ so only the following can be used for RX:
+ 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
+
+ Not all pins on the Leonardo support change interrupts,
+ so only the following can be used for RX:
+ 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
+
+ created back in the mists of time
+ modified 25 May 2012
+ by Tom Igoe
+ based on Mikal Hart's example
+
+ This example code is in the public domain.
+
+ */
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
-// LED connected to digital pin 13
-const int led_pin =  13;
-// Blink interval in milliseconds
-const unsigned long interval = 1000;
 
-unsigned long prev_time = 0;
-int led_state = LOW;
+const int RXArdu = 2;
+const int TXArdu = 3;
+const int baudrate_uart = 9600;
 
-void setup()   {
-    // Initialize the digital pin as an output:
-    pinMode(led_pin, OUTPUT);
+SoftwareSerial UART(RXArdu, TXArdu); // RX, TX
+
+uint8_t co2_level_cmd[9] = {0xFF, // starting byte
+                            0x01, // sensor no
+                            0x86, // command
+                            0x00,
+                            0x00,
+                            0x00,
+                            0x00,
+                            0x00,
+                            0x79};
+
+uint8_t answer[9]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+void printMessage(uint8_t *message, int bytes) {
+    if (message[1] == 0x86) {
+        Serial.print("Co2 level message received: ");
+        uint8_t highLevel = message[2];
+        uint8_t lowLevel = message[3];
+        int co2level = highLevel * 256 + lowLevel;
+        Serial.print(co2level);
+        Serial.print(" ppm\n");
+    }
+}
+void resetAnswer(uint8_t * x){
+    for (int i = 0; i < 9; ++i) {
+        x[i]=0;
+    }
 }
 
-void loop()
-{
-    const unsigned long cur_time = millis();
 
-    if(cur_time - prev_time > interval)
-    {
-        prev_time = cur_time;
-        led_state = led_state == LOW ? HIGH : LOW;
-        // Write output
-        digitalWrite(led_pin, led_state);
+void setup() {
+    // Open serial communications and wait for port to open:
+    Serial.begin(115200);
+    while (!Serial) {  // wait for serial port to connect. Needed for Native USB only
     }
+
+
+    Serial.println("Serial Connected");
+
+    // set the data rate for the SoftwareSerial port
+    UART.begin(baudrate_uart);
+
+}
+
+void loop() // run over and over
+{
+
+    resetAnswer(answer);
+    UART.write(co2_level_cmd, 9);
+    UART.readBytes(answer, 9);
+
+    Serial.print("I just read: ");
+    for (int i = 0; i < 9; ++i) {
+        Serial.print(answer[i], HEX);
+        Serial.print(" ");
+    }
+    Serial.print('\n');
+    printMessage(answer,9);
+    delay(1000);
 }
