b76f74cf71d59a75e1cb11d56f4bac3dfd8b8a4c
jplaza <jplazarenas@gmail.com>
2020-12-03 17:30:47 +0100

    Version para esp_8266_mini con funcionalidad wifi y serial. Codigo no optimizado

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -7,12 +7,21 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
+#include "pass.hpp"
 
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+
+/*Sensor*/
 const int RXArdu = 13;//7;//15;//13;//3;//15;
 const int TXArdu = 15;//8;//16;//15;//1;//16;
 const int baudrate_uart = 9600;
 
 SoftwareSerial UART(RXArdu, TXArdu); // RX, TX
+
+int co2_ppm_=0;
 
 uint8_t co2_level_cmd[9] = {0xFF, // starting byte
                             0x01, // sensor no
@@ -26,15 +35,18 @@ uint8_t co2_level_cmd[9] = {0xFF, // starting byte
 
 uint8_t answer[9]{0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-void printMessage(uint8_t *message, int bytes) {
-    if (message[1] == 0x86) {
+int printMessage(uint8_t *message, int bytes) {
+    if (message[1] == 0x86) 
+    {
         Serial.print("Co2 level message received: ");
         uint8_t highLevel = message[2];
         uint8_t lowLevel = message[3];
         int co2level = highLevel * 256 + lowLevel;
+        co2_ppm_=co2level;
         Serial.print(co2level);
         Serial.print(" ppm\n");
     }
+    return co2_ppm_;
 }
 void resetAnswer(uint8_t * x){
     for (int i = 0; i < 9; ++i) {
@@ -42,24 +54,75 @@ void resetAnswer(uint8_t * x){
     }
 }
 
+/*Conexion wifi y mqtt*/
+WiFiClient client;
 
-void setup() {
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+Adafruit_MQTT_Publish co2_concentracion = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/co2_concentracion");
+
+
+
+void setup() 
+{
+    /*Comunicacion serie*/
     // Open serial communications and wait for port to open:
     Serial.begin(115200);
-    while (!Serial) {  // wait for serial port to connect. Needed for Native USB only
-    }
-
-
+    while (!Serial) { }
     Serial.println("Serial Connected");
 
+    /*Sensor*/
     // set the data rate for the SoftwareSerial port
     UART.begin(baudrate_uart);
+
+    /*Conexion wifi*/
+    WiFi.begin(R_SSID, PASS);
+    int counter=10;
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+        delay(500);
+        Serial.print(".");
+        counter--;
+        if(counter==0)
+        {
+            // reset me
+            Serial.println("Conexion Wifi 1 FALLIDA... reseteando...");
+            ESP.reset();
+        }
+    }
+    Serial.println("Conexion Wifi 1 EXITOSA");
 
 }
 
 void loop() // run over and over
 {
 
+    /*Mqtt server*/
+    /*if (mqtt.connected()) 
+    {
+        Serial.println("Conexion MQTT FALLIDA... saliendo del programa...");
+        delay(5000);
+        //return;       //¿ENTIENDO QUE ESTE RETURN ES POR SI NO SEGUIMOS CONECTADOS FINALIZAR EL PROGAMA 
+    }*/
+
+    uint8_t retries = 5;
+    int ret=0;
+    while ((ret = mqtt.connect()) != 0)  // connect will return 0 for connected  
+    {
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Reintentando conexion MQTT... 5 segundos ...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) 
+       {
+            // reset me
+            Serial.println("Reconexion MQTT FALLIDA... reseteando...");
+            ESP.reset();
+       }
+    }
+
+    /*Sensor*/
     resetAnswer(answer);
     UART.write(co2_level_cmd, 9);
     UART.readBytes(answer, 9);
@@ -70,10 +133,24 @@ void loop() // run over and over
         Serial.print(" ");
     }
     Serial.print('\n');
-    printMessage(answer,9);
-    delay(1000);
-}
+    co2_ppm_=printMessage(answer,9);
+    Serial.print("COMPROBANDO VALOR CO2: ");
+    Serial.println(co2_ppm_);
+    //delay(1000);
 
+    /*Envio de datos por wifi y mqtt*/
+    if (! co2_concentracion.publish(co2_ppm_)) 
+    { 
+        Serial.println("Envio wifi de co2 FALLIDO...");
+    } 
+    else 
+    {
+        Serial.println("Envio wifi de co2 EXITOSO");
+    }
+
+    delay(15000);
+
+}
 
 
 /*
